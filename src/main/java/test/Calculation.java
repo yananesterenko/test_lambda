e65de840661eaacc33fcf6d5ff46b17ecc22f544
yananesterenko <nesterenko.yana@gmail.com>
2021-04-20 15:36:43 +0200

    first commit

---
+++ src/main/java/test/Calculation.java
@@ -0,0 +1,10 @@
+package test;
+
+public class Calculation {
+
+    public int add(int a, int b){
+
+        return a+b;
+
+    }
+}
---
+++ src/main/java/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+
+        String a= "test";
+        a.contains("a");
+
+        Calculation c = new Calculation();
+        System.out.println(c.add(2,2));
+    }
+
+}
